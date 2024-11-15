cbad5e3bb097fb3fb9c267d13905c86d00525de5
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-15 06:01:45 -0600

    refactored medifiles added url  for detail view and api

--- backend/property/urls.py
+++ backend/property/urls.py
@@ -3,5 +3,6 @@ from . import api
 
 urlpatterns = [
     path('properities/', api.properties_list, name='api_properties_list'),
-    path('properties/create/', api.create_property, name='api_create')
+    path('properties/create/', api.create_property, name='api_create'),
+    path('properties/<uuid:pk>/', api.properties_derail, name='api_properties_detail')
 ]
