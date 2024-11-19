46d7263322b6a4f2773e7a379b127e6f83abe0d0
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-19 07:48:54 -0600

    refactored url for correct working

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -4,6 +4,6 @@ from . import api
 
 urlpatterns = [
     path('', api.conversation_list, name='api_conversations_list'),
-    path('start/<uuid:user_id>', api.conversation_start, name='api_conversation_start'),
+    path('start/<uuid:user_id>/', api.conversation_start, name='api_conversation_start'),
     path('<uuid:pk>/', api.conversations_detail, name='api_conversations_detail')
 ]
