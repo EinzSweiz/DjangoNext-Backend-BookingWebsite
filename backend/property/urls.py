389c03b1266c1571aa6bc07ee1fe1b8553ef5188
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-21 04:53:21 -0600

    corrected properties list url path

--- backend/property/urls.py
+++ backend/property/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path('properities/', api.properties_list, name='api_properties_list'),
+    path('properties/', api.properties_list, name='api_properties_list'),
     path('properties/create/', api.create_property, name='api_create'),
     path('properties/<uuid:pk>/', api.properties_detail, name='api_properties_detail'),
     path('properties/<uuid:pk>/book/', api.book_property, name='api_properties_book'),
