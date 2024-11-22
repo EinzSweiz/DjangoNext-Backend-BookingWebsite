df2658ad1948894985f4a3697c3673d0a8232a93
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-22 08:00:15 -0600

    created profile view for user and added update profile with put method

--- backend/useraccounts/api.py
+++ backend/useraccounts/api.py
@@ -1,4 +1,4 @@
-from .serializers import UserDetailSerializer
+from .serializers import UserDetailSerializer, UserProfileUpdateSerializer, UserProfileSerializer
 from django.http import JsonResponse
 from .models import User
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -18,3 +18,23 @@ def reservations_list(request):
     qs = request.user.reservations.all()
     serializer = ResirvationListSerializer(qs, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def profile_detail(request):
+    serializer = UserProfileSerializer(request.user)
+    return JsonResponse(serializer.data)
+
+
+@api_view(['PUT'])
+@authentication_classes([])
+@permission_classes([])
+def update_profile(request):
+    user = request.user
+    serializer = UserProfileUpdateSerializer(user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data)
+    return JsonResponse(serializer.errors, status=400)
