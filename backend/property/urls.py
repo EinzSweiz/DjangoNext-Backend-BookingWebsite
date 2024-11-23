822b0b4eb95ad488b0442deb96713117746e4163
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-23 06:40:51 -0600

    refactored code a few more for favorites

--- backend/property/urls.py
+++ backend/property/urls.py
@@ -3,7 +3,6 @@ from . import api
 
 urlpatterns = [
     path('properties/', api.properties_list, name='api_properties_list'),
-    path('proeprties/<uuid:pk>/favorited/', api.user_favorites, name='api_properties_favorited'),
     path('properties/create/', api.create_property, name='api_create'),
     path('properties/<uuid:pk>/', api.properties_detail, name='api_properties_detail'),
     path('properties/<uuid:pk>/book/', api.book_property, name='api_properties_book'),
