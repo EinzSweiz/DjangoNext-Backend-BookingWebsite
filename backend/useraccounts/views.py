001ae431ffa3229ff8723fbffa9a654d42ccb7f2
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-26 09:31:09 -0600

    added email model for checking verified or not, chaned confirmation url to frontend and will receive the data from query for solving the confirmation from frontend

--- backend/useraccounts/views.py
+++ backend/useraccounts/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
-from .models import User
+from .models import User, Email
 from dj_rest_auth.views import LoginView
 from rest_framework.exceptions import AuthenticationFailed
 
@@ -17,6 +17,10 @@ def confirm_email(request, uidb64, token):
             user.is_active = True  # Activate the user
             user.is_verified = True
             user.save()
+
+            email = Email.objects.get(user=user)
+            email.verified = True
+            email.save()
             return JsonResponse({"message": "Email confirmed successfully"}, status=200)
         else:
             return JsonResponse({"error": "Invalid or expired token"}, status=400)
