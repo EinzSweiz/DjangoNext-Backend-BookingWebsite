3c64a264d5b3c9bccc2e7d97f9184eac34b5be5f
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-27 03:52:07 -0600

    added logger

--- backend/useraccounts/api.py
+++ backend/useraccounts/api.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 from .models import User
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from property.serializers import ResirvationListSerializer
+import logging
 
+logger = logging.getLogger(__name__)
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
@@ -20,24 +22,21 @@ def reservations_list(request):
     return JsonResponse(serializer.data, safe=False)
 
 
-@api_view(['GET'])
-@authentication_classes([])
-@permission_classes([])
 def profile_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
-        print(vars(user))
+        logger.debug(f"User details: {vars(user)}")
         for user in User.objects.all():
             try:
-                str(user.name)  # Force decode fields to UTF-8
+                str(user.name)
                 str(user.email)
             except UnicodeDecodeError:
-                print(f"Problematic user: {user.id}")
+                logger.error(f"Problematic user: {user.id}")
         serializer = UserProfileSerializer(user)
         return JsonResponse(serializer.data)
     except UnicodeDecodeError as e:
+        logger.error(f"Encoding error: {e}")
         return JsonResponse({"error": f"Encoding error: {e}"}, status=400)
-
 
 
 @api_view(['PUT'])
