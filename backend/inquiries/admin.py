8651e494874b923d53584978d17b88c802bb3f1a
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-28 12:36:46 -0600

    added admin for Inquiry

--- backend/inquiries/admin.py
+++ backend/inquiries/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Inquiry
 
-# Register your models here.
+
+
+@admin.register(Inquiry)
+class InquiryAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'message', 'response', 'status', 'created_at']
