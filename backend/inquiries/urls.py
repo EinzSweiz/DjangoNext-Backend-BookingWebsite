574201ebe97a55fe1275b2f252c90d200f9699b1
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-29 04:53:40 -0600

    added detal view(demo for now)

--- backend/inquiries/urls.py
+++ backend/inquiries/urls.py
@@ -4,4 +4,5 @@ from . import api
 urlpatterns = [
     path('create/', api.create_inquiry, name='api_create_inquiry'),
     path('get/', api.inquiries_view, name='api_inquiries_view'),
+    path('get/<int:pk>/', api.inquiry_detail_api, name='api_detail_view'),
 ]
