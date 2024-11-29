63ff519a440d6292663766ab607080aa0c22bc11
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-29 11:47:03 -0600

    updated api's and added new urls refactored serializers

--- backend/inquiries/api.py
+++ backend/inquiries/api.py
@@ -4,7 +4,7 @@ from .models import Inquiry
 from useraccounts.models import User
 from rest_framework.parsers import JSONParser
 from django.http import Http404
-from .serializers import CreateInquirySerializer, GetInquirySerializer, MessageSerializer
+from .serializers import CreateInquirySerializer, GetInquirySerializer, MessageSerializer, UpdateStatusSerializer
 
 
 @api_view(['POST'])
@@ -30,38 +30,46 @@ def inquiries_view(request):
     inquiries_serializer = GetInquirySerializer(inquiries, many=True)
     return JsonResponse(inquiries_serializer.data, safe=False)
 
-def handle_get_request(inquiry):
-    serializer = GetInquirySerializer(inquiry)
-    return JsonResponse(serializer.data, status=200)
-
-def handle_put_request(request, inquiry):
-    data = JSONParser().parse(request)
-    serializer = GetInquirySerializer(inquiry, data=data, partial=False)
-    if serializer.is_valid():
-        serializer.save()
-        return JsonResponse(serializer.data, status=200)
-    return JsonResponse(serializer.errors, status=400)
-
-def handle_post_request(request, inquiry):
-    data = JSONParser().parse(request)
-    message_data = {'inquiry': inquiry.id, 'sender': data.get('sender'), 'message': data.get('message')}
-    message_serializer = MessageSerializer(data=message_data)
-    if message_serializer.is_valid():
-        message_serializer.save()
-        return JsonResponse({'message': 'Message added successfully'}, status=201)
-    return JsonResponse(message_serializer.errors, status=400)
-
-@api_view(['GET', 'PUT', 'POST'])
-def inquiry_detail_api(request, pk):
+@api_view(['POST'])
+def add_message(request, pk):
     try:
         inquiry = Inquiry.objects.get(pk=pk)
-        if request.method == 'GET':
-            return handle_get_request(inquiry)
-        elif request.method == 'PUT':
-            return handle_put_request(request, inquiry)
-        elif request.method == 'POST':
-            return handle_post_request(request, inquiry)
+        data = JSONParser().parse(request)
+        message_data = {'inquiry': inquiry.id, 'sender': data.get('sender'), 'message': data.get('message')}
+        message_serializer = MessageSerializer(data=message_data)
+
+        if message_serializer.is_valid():
+            message_serializer.save()
+            return JsonResponse({'message': 'Message added successfully'}, status=201)
+        return JsonResponse(message_serializer.errors, status=400)
+    
     except Inquiry.DoesNotExist:
-        raise Http404("Inquiry not found")
-    except Exception as e:
-        return JsonResponse({'error': 'An unexpected error occurred.', 'details': str(e)}, status=500)
+        return JsonResponse({"error": "Inquiry not found"}, status=404)
+    
+
+
+@api_view(['PUT'])
+def update_inquiry_status(request, pk):
+    try:
+        inquiry = Inquiry.objects.get(pk=pk)
+        data = JSONParser().parse(request)
+        serializer = UpdateStatusSerializer(inquiry, data=data, partial=True)  # partial=True allows for partial updates
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+
+    except Inquiry.DoesNotExist:
+        return JsonResponse({"error": "Inquiry not found"}, status=404)
+    
+
+
+@api_view(['GET'])
+def get_inquiry(request, pk):
+    try:
+        inquiry = Inquiry.objects.get(pk=pk)
+        serializer = GetInquirySerializer(inquiry)
+        return JsonResponse(serializer.data, status=200)
+    except Inquiry.DoesNotExist:
+        return JsonResponse({"error": "Inquiry not found"}, status=404)
