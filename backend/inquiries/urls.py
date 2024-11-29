d3906ab5a71c790f85a41b9c9dbef21f3560d8dc
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-29 11:47:58 -0600

    update

--- backend/inquiries/urls.py
+++ backend/inquiries/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('get/', api.inquiries_view, name='api_inquiries_view'),
     path('get/<int:pk>/', api.inquiry_detail_api, name='api_detail_view'),
     path('add-message/<int:pk>/', api.add_message, name='api_add_message'),
-    path('update_status/<int:pk>/', api.update_inquiry_status, name='api_update_status')
+    path('update-status/<int:pk>/', api.update_inquiry_status, name='api_update_status')
 ]
