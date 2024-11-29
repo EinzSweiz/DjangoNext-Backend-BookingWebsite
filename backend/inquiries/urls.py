84b1ae5268a588c80032b58d6fec1b2fd307a3c8
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-29 02:48:15 -0600

    changed url for get inquirires

--- backend/inquiries/urls.py
+++ backend/inquiries/urls.py
@@ -3,5 +3,5 @@ from . import api
 
 urlpatterns = [
     path('create/', api.create_inquiry, name='api_create_inquiry'),
-    path('get', api.inquiries_view, name='api_inquiries_view'),
+    path('get/', api.inquiries_view, name='api_inquiries_view'),
 ]
