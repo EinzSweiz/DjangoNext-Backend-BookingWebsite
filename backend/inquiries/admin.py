c40435d0b59be5bce8c258afdd711aefbbf505a5
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-29 06:26:48 -0600

    added email to serializer and admin panel

--- backend/inquiries/admin.py
+++ backend/inquiries/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Inquiry
+from .models import Inquiry, Message
 
 
 
 @admin.register(Inquiry)
 class InquiryAdmin(admin.ModelAdmin):
     list_display = ['subject', 'message', 'response', 'status', 'created_at']
+
+@admin.register(Message)
+class InquiryAdmin(admin.ModelAdmin):
+    list_display = ['inquiry', 'sender', 'message', 'timestamp']
