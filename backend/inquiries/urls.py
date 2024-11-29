bf2c810c22e08a340f47febd491ee3d1eb274e4e
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-29 11:51:12 -0600

    refactored urls

--- backend/inquiries/urls.py
+++ backend/inquiries/urls.py
@@ -4,7 +4,7 @@ from . import api
 urlpatterns = [
     path('create/', api.create_inquiry, name='api_create_inquiry'),
     path('get/', api.inquiries_view, name='api_inquiries_view'),
-    path('get/<int:pk>/', api.inquiry_detail_api, name='api_detail_view'),
+    path('get/<int:pk>/', api.get_inquiry, name='api_detail_view'),
     path('add-message/<int:pk>/', api.add_message, name='api_add_message'),
     path('update-status/<int:pk>/', api.update_inquiry_status, name='api_update_status')
 ]
