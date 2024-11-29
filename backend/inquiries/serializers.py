92fc3ab26198bb79113451069c049d86f19e0305
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-29 11:47:03 -0600

    updated api's and added new urls refactored serializers

--- backend/inquiries/serializers.py
+++ backend/inquiries/serializers.py
@@ -25,7 +25,7 @@ class CreateInquirySerializer(serializers.ModelSerializer):
 class MessageSerializer(serializers.ModelSerializer):
     sender = serializers.CharField()
     message = serializers.CharField()
-    timestamp = serializers.DateTimeField()
+    timestamp = serializers.DateTimeField(required=False)
 
     class Meta:
         model = Message
@@ -39,4 +39,16 @@ class GetInquirySerializer(serializers.ModelSerializer):
     class Meta:
         model = Inquiry
         fields = ('id', 'subject', 'message', 'response', 'status', 'created_at', 'updated_at', 'user_name', 'messages', 'user_email')
+
+
+class UpdateStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inquiry
+        fields = ('id', 'status')
+
+    def validate_status(self, value):
+        valid_statuses = ['open', 'in_progress', 'closed']  # Add valid statuses for your business logic
+        if value not in valid_statuses:
+            raise serializers.ValidationError(f"Invalid status: {value}. Valid statuses are {valid_statuses}.")
+        return value
 
--- backend/inquiries/urls.py
+++ backend/inquiries/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('create/', api.create_inquiry, name='api_create_inquiry'),
     path('get/', api.inquiries_view, name='api_inquiries_view'),
     path('get/<int:pk>/', api.inquiry_detail_api, name='api_detail_view'),
+    path('add-message/<int:pk>/', api.add_message, name='api_add_message'),
+    path('update_status/<int:pk>/', api.update_inquiry_status, name='api_update_status')
 ]
