0d7856ac5c61b826f5025bedd0eed95b8b4f3b81
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-30 09:25:16 -0600

    added serializer for booking

--- backend/property/serializers.py
+++ backend/property/serializers.py
@@ -35,3 +35,10 @@ class ResirvationListSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'start_date', 'end_date', 'number_of_nights', 'total_price', 'property'
         )
+
+class BookingSerializer(serializers.Serializer):
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
+    total_price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    number_of_nights = serializers.IntegerField()
+    guests = serializers.IntegerField()
