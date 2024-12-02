a01cac8e2a162cd824df971cb057236104d6116e
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-02 06:00:09 -0600

    refactored for proper working

--- backend/my_stripe/urls.py
+++ backend/my_stripe/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import api
 urlpatterns = [
-    path('payment/success/', api.payment_success, name='api_payment_success')
+    path('payment/success/', api.payment_success, name='api_payment_success'),
+    path('payment/cancel/', api.payment_cancel, name='api_payment_cancel')
 ]
