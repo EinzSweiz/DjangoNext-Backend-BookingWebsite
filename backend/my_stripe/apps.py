2d100d100ba40799725b043fa906b1cd72f15aff
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-02 04:41:21 -0600

    added new model and app for stripe payment

---
+++ backend/my_stripe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyStripeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_stripe'
---
+++ backend/my_stripe/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import stripe
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def product_checkout_view(request, property, pk, total_price, start_date, end_date, number_of_nights, guests):
+    checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',  # You can change the currency
+                        'product_data': {
+                            'name': property.title,
+                        },
+                        'unit_amount': int(float(total_price) * 100),  # Stripe expects amount in cents
+                    },
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            payment_method_types=['card'],
+            customer_creation='always',
+            success_url=request.build_absolute_uri(f'/payment/success/?session_id={{CEHCKOUT_SESSION_ID}}/'),  # Redirect to success URL after payment
+            cancel_url=request.build_absolute_uri(f'/payment/cancel/{pk}/'),  # Redirect to cancel URL if payment fails
+            customer_email=request.user.email,  # Optional: Prefill user email in checkout
+            metadata={
+                'property_id': pk,
+                'start_date': start_date,
+                'end_date': end_date,
+                'number_of_nights': number_of_nights,
+                'guests': guests,
+                'total_price': total_price,
+            }
+        )
+    return checkout_session
