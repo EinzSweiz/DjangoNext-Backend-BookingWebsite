842831946e7779fb5453f39676218dc1a9638175
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-03 03:02:09 -0600

    fixed the bugs

--- backend/property/serializers.py
+++ backend/property/serializers.py
@@ -42,4 +42,4 @@ class BookingSerializer(serializers.Serializer):
     total_price = serializers.DecimalField(max_digits=10, decimal_places=2)
     number_of_nights = serializers.IntegerField()
     guests = serializers.IntegerField()
-    has_paid = serializers.BooleanField()
+    has_paid = serializers.BooleanField(required=False, default=False)  # Add this field
