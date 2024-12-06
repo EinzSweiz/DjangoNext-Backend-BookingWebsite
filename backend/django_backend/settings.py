6715b8df766ddbcef4ab72d5ee3b8216352cd68e
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-06 12:06:02 -0600

    will try

--- backend/django_backend/settings.py
+++ backend/django_backend/settings.py
@@ -162,7 +162,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.sites',
+    'django.contrib.sites',
     #installed app
     'channels',
     'rest_framework',
