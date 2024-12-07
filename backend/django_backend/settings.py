e6d7f6ae02921dc612878534d21bf3d3058771ec
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-07 03:13:02 -0600

    added login redirect to frontend and fixed code for google

--- backend/django_backend/settings.py
+++ backend/django_backend/settings.py
@@ -106,10 +106,11 @@ SOCIALACCOUNT_PROVIDERS = {
         'VERIFIED_EMAIL': True,
     }
 }
+SOCIALACCOUNT_LOGIN_ON_GET = True
 USE_X_FORWARDED_HOST = True
 GOOGLE_OAUTH2_REDIRECT_URI = 'https://api.diplomaroad.pro/accounts/google/login/callback/'
 ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "https://www.diplomaroad.pro/home/"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         'rest_framework_simplejwt.authentication.JWTAuthentication',    
