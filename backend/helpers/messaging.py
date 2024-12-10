e44adb3f4d272fb9edee16d620ab1cd33c4718d4
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-10 06:58:17 -0600

    added html message

--- backend/helpers/messaging.py
+++ backend/helpers/messaging.py
@@ -1,6 +1,6 @@
 from django.core.mail import EmailMessage
 
-def send_message(subject, message, from_email, to_email, attachment_name=None, attachment_content=None, attachment_mime_type=None):
+def send_message(subject, message, from_email, to_email, attachment_name=None, attachment_content=None, attachment_mime_type=None, html_message=None):
     # Create the email message
     email = EmailMessage(
         subject=subject,
@@ -8,8 +8,9 @@ def send_message(subject, message, from_email, to_email, attachment_name=None, a
         from_email=from_email,
         to=[to_email]
     )
-    email.content_subtype = 'html'  # Set content type to HTML
-
+    if html_message:
+        email.content_subtype = 'html'  # Set content type to HTML
+        email.body = html_message  # Use the HTML content
     # Attach the file if provided
     if attachment_name and attachment_content:
         email.attach(attachment_name, attachment_content, attachment_mime_type)
