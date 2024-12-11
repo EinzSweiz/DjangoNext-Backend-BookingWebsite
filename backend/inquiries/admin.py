bd443d9b981fe7f0681f6d4774a7654f50a9caa9
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-11 07:53:32 -0600

    ..

--- backend/inquiries/admin.py
+++ backend/inquiries/admin.py
@@ -5,7 +5,7 @@ from .models import Inquiry, Message
 
 @admin.register(Inquiry)
 class InquiryAdmin(admin.ModelAdmin):
-    list_display = ['subject', 'message', 'resolution_steps', 'status', 'created_at']
+    list_display = ['subject', 'message', 'response', 'status', 'created_at']
 
 @admin.register(Message)
 class InquiryAdmin(admin.ModelAdmin):
