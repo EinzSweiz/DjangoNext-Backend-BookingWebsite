0e809b9034bcac63bc50f4175c9be269a0673988
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-17 09:22:00 -0600

    ...

--- backend/django_backend/asgi.py
+++ backend/django_backend/asgi.py
@@ -8,6 +8,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
 
+application = get_asgi_application()
 
 from chat import routing
 from chat.token_auth import TokenAuthMiddleware
