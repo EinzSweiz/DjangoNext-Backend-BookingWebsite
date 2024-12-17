683be39672a6b8534aad892c851afd9e595fdc4d
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-17 09:00:42 -0600

    added healthcheck to daphne

--- backend/django_backend/asgi.py
+++ backend/django_backend/asgi.py
@@ -8,7 +8,6 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
 
-application = get_asgi_application()
 
 from chat import routing
 from chat.token_auth import TokenAuthMiddleware
