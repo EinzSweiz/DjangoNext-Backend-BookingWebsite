1627de47ebf22af7f9276bddd3149bb222b82f3e
EinzSweiz <riad.sultanov.1999@gmail.com>
2025-01-01 06:38:54 -0600

    ..

--- backend/my_stripe/urls.py
+++ backend/my_stripe/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import api
 urlpatterns = [
-    path('payment/success/', api.payment_success, name='api_payment_success'),
-    path('payment/cancel/', api.payment_cancel, name='api_payment_cancel'),
+    path('payment/success/', api.PaymentSuccessAPIView.as_view(), name='api_payment_success'),
+    path('payment/cancel/', api.PaymentCancelAPIView.as_view(), name='api_payment_cancel'),
     path('stripe_webhook/', api.stripe_webhook, name='stripe_webhook'),
 ]
